c49aa7480edfe0015c3b3b11d6ae9a20500ff4a5
Marcus Peterson <mdabomb5@gmail.com>
2021-09-27 15:24:08 -0600

    added communities

--- utils/lookup.js
+++ utils/lookup.js
@@ -3,7 +3,11 @@ const courseLookup = {
 }
 
 const communityLookup = {
-    'the-opportunity-network': 'C026BELRBPX'
+    'the-opportunity-network': 'C026BELRBPX',
+    'tech-moms': 'C02EMTFR3A8',
+    'utah-health': 'C02FMNXTYKY',
+    'meritorious': 'C02FDPWS0F9',
+    'apartment-housing-rentals-foundation': 'C02GJ4KUBEC'
 }
 
 module.exports = {
